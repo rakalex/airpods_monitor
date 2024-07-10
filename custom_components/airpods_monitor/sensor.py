72b2439d27617a06680dccf6fd93ada65f311ae1
rakalex <rak.alexei@gmail.com>
2024-07-10 07:39:39 +0300

    Update sensor.py

--- custom_components/airpods_monitor/sensor.py
+++ custom_components/airpods_monitor/sensor.py
@@ -1,50 +1,39 @@
-"""Sensor platform for AirPods Monitor."""
-import logging
 from homeassistant.components.sensor import SensorEntity
-from homeassistant.const import STATE_UNKNOWN
-from homeassistant.helpers.entity_platform import async_add_entities
-
-_LOGGER = logging.getLogger(__name__)
+from .airpods import get_data
 
 async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
-    """Set up the AirPods Monitor sensor."""
-    coordinator = hass.data["airpods_monitor"]
+    async_add_entities([
+        AirPodsBatterySensor("AirPods Left Battery"),
+        AirPodsBatterySensor("AirPods Right Battery"),
+        AirPodsBatterySensor("AirPods Case Battery"),
+    ])
 
-    async_add_entities([AirPodsSensor(coordinator)], True)
+class AirPodsBatterySensor(SensorEntity):
 
-async def async_setup_entry(hass, entry, async_add_entities):
-    """Set up AirPods Monitor sensor from a config entry."""
-    coordinator = hass.data["airpods_monitor"][entry.entry_id]
-
-    async_add_entities([AirPodsSensor(coordinator)], True)
-
-class AirPodsSensor(SensorEntity):
-    """Representation of a Sensor."""
-
-    def __init__(self, coordinator):
-        """Initialize the sensor."""
-        self._coordinator = coordinator
-        self._state = STATE_UNKNOWN
-        self._data = {}
+    def __init__(self, name):
+        self._name = name
+        self._state = None
+        self._attr = None
 
     @property
     def name(self):
-        """Return the name of the sensor."""
-        return "AirPods Monitor"
+        return self._name
 
     @property
     def state(self):
-        """Return the state of the sensor."""
         return self._state
 
     @property
     def extra_state_attributes(self):
-        """Return the state attributes."""
-        return self._data
+        return self._attr
 
-    async def async_update(self):
-        """Fetch new state data for the sensor."""
-        await self._coordinator.async_request_refresh()
-        data = self._coordinator.data
-        self._data = data
-        self._state = data.get("status", STATE_UNKNOWN)
+    def update(self):
+        data = get_data()
+        if data["status"] == 1:
+            self._attr = data
+            if self._name == "AirPods Left Battery":
+                self._state = data["charge"]["left"]
+            elif self._name == "AirPods Right Battery":
+                self._state = data["charge"]["right"]
+            elif self._name == "AirPods Case Battery":
+                self._state = data["charge"]["case"]
